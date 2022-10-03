b43c07c6ca6ad854e7f750a9ddc23504eefb9b21
Royal_Telivision <gwenom64@gmail.com>
2022-10-03 11:26:44 -0400

    homework

--- week03/homework palaestra.py
+++
@@ -1,3 +0,0 @@
-x= "help"
-y= "her"
-print( x, y)
--- week03/variables.py
+++ week03/variables.py
@@ -1,3 +1,20 @@
-x= "help"
-y= "her"
-print( x, y)
+score=0
+answer= input("what color is the sky")
+if answer == "blue":
+    score=score + 1
+    print("Yes!")
+else:
+    print("I was looking for 'blue'")
+answer= input("How old is the president?")
+if answer== "79":
+    print("Good.")
+    score= score+1
+else:
+    print("Nope.")
+answer= input("How many Great Lakes are there?")
+if answer == "5":
+    score= score+1
+    print("Oh, you are smart!")
+else:
+    print("Nice try.")
+print("Your score is", score)
