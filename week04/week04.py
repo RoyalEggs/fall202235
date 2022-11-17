ca1668433b0614dd7ca929bb28d8105a2847f3cd
Royal_Telivision <gwenom64@gmail.com>
2022-11-17 10:35:46 -0500

    Week04 again but fixed

--- week04/week04.py
+++ week04/week04.py
@@ -1,36 +1,36 @@
 score=0
-answer= input("what color is the sky")
+answer= input("what color is the sky?  ")
 if answer == "blue":
     score=score + 1
     print("Yes!")
 else:
     print("I was looking for 'blue'")
-answer= input("How old is the president?")
+answer= input("How old is the president?   ")
 if answer== "79":
     print("Good.")
     score= score+1
 else:
     print("Nope.")
-answer= input("How many Great Lakes are there?")
+answer= input("How many Great Lakes are there?    ")
 if answer == "5":
     score= score+1
     print("Oh, you are smart!")
 else:
     print("Nice try.")
-answer= input("What is the answer to everything?")
+answer= input("What is the answer to everything?   ")
 if answer== "Jesus":
     score=score+1
     print("Always will be.")
 else:
     print("No sir.")
-answer= input("What color is a carrot?")
-if answer == ("I think its a carrot"):
+answer= input("What color is a carrot?     ")
+if answer == ("A carrot"):
     score=score+1
     print("Its a carrot.")
 else:
     print("No.")
-answer= input("If you dig a six foot hole, how deep is that hole?")
-if answer== ("20 ft"):
+answer= input("If you dig a six foot hole, how deep is that hole?    ")
+if answer== ("20ft"):
     score=score+1
     print("Yes")
 else:
