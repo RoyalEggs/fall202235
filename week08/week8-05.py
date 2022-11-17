f279684ef192d77a9d36e9aac52a48f0b95f3d67
Royal_Telivision <gwenom64@gmail.com>
2022-11-17 10:30:50 -0500

    Week08 homework

--- week08/week8-05.py
+++ week08/week8-05.py
@@ -2,8 +2,13 @@ import os
 
 sourceDir = os.path.dirname(__file__)
 
-f = open(os.path.join(sourceDir, "week08-05-input.txt"))
+f = open(os.path.join(sourceDir, "week8-output.txt"))
 lines = f.readlines()
-for line in lines: 
-    print(f"Words, {line.strip()}")
-f.close()
+words = (" ")
+words = lines
+for word in words:
+    print({word.strip()})
+    count = len(words)
+if word.strip() !=" ":
+    print(f"words: {len(words)}")
+f.close
